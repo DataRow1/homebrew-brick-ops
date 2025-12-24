61524c591540bac9098d34a40855fd851f187ef4
Rowan <rowan.weiffenbach@me.com>
2025-12-24 16:13:26 +0100

    Update brick-ops.rb

--- Formula/brick-ops.rb
+++ Formula/brick-ops.rb
@@ -3,8 +3,8 @@ class BrickOps < Formula
 
   desc "Databricks operations CLI"
   homepage "https://github.com/DataRow1/db-ops"
-  url "https://github.com/DataRow1/db-ops/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "ad668ba00a4dff945c37646bc88c42c25a356640681fb3032cc5fcdadb3351b6"
+  url "https://github.com/DataRow1/db-ops/archive/refs/tags/0.1.2.tar.gz"
+  sha256 "eb9c4c1f88a3e64e78edadd49aadd90ed97dbd128f89d0dc4563d127520e9cd8"
   license "MIT"
 
   depends_on "python@3.11"
