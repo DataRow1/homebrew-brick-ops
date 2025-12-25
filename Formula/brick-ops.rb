4d7444228cdab53ffaba97b592ed1df86ec8b5ad
Rowan <rowan.weiffenbach@me.com>
2025-12-25 10:10:32 +0100

    Update brick-ops.rb

--- Formula/brick-ops.rb
+++ Formula/brick-ops.rb
@@ -1,15 +1,15 @@
 class BrickOps < Formula
   desc "Databricks operations CLI for Jobs and Unity Catalog"
-  homepage "https://github.com/DataRow1/brick-ops"
+  homepage "https://github.com/DataRow1/dbops"
   license "MIT"
   version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DataRow1/brick-ops/releases/download/v0.1.3/dbops-darwin-arm64"
+      url "https://github.com/DataRow1/brick-ops/releases/download/v0.1.4/dbops-darwin-arm64"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/DataRow1/brick-ops/releases/download/v0.1.3/dbops-darwin-amd64"
+      url "https://github.com/DataRow1/brick-ops/releases/download/v0.1.4/dbops-darwin-amd64"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
