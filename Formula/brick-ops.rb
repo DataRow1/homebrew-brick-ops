36da921388a6c64929d45cae7309fef342367c9c
Rowan <rowan.weiffenbach@me.com>
2025-12-25 10:23:44 +0100

    Update brick-ops.rb

--- Formula/brick-ops.rb
+++ Formula/brick-ops.rb
@@ -6,10 +6,10 @@ class BrickOps < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DataRow1/db-ops/releases/download/v0.1.4/dbops-darwin-arm64"
+      url "https://github.com/DataRow1/db-ops/releases/download/0.1.4/dbops-darwin-arm64"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/DataRow1/db-ops/releases/download/v0.1.4/dbops-darwin-amd64"
+      url "https://github.com/DataRow1/db-ops/releases/download/0.1.4/dbops-darwin-amd64"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
