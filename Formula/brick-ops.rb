be5f519e7837f83a77ce8ef51d8957e391fa18c2
DataRow1 <17173370+DataRow1@users.noreply.github.com>
2026-01-05 14:33:51 +0000

    brick-ops 0.1.10

--- Formula/brick-ops.rb
+++ Formula/brick-ops.rb
@@ -2,21 +2,19 @@ class BrickOps < Formula
   desc "Databricks operations CLI for Jobs and Unity Catalog"
   homepage "https://github.com/DataRow1/brick-ops"
   license "MIT"
-  version "0.1.7"
+  version "0.1.10"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/dbops-darwin-arm64.tar.gz"
-      sha256 "__DARWIN_ARM64_SHA256__"
+      url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/8d44637f77540961ec45446cd60b15ac1e4a669af1b9fd587e3d44e2004246a1"
     else
-      url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/dbops-darwin-amd64.tar.gz"
-      sha256 "__DARWIN_AMD64_SHA256__"
+      url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/e7c8057946018a6c9c7e4259c37927b6eeaffde4f8f3a675665c1b568c32e83"
     end
   end
 
   on_linux do
     url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/dbops-linux-amd64.tar.gz"
-    sha256 "__LINUX_AMD64_SHA256__"
+    sha256 "e639cbf7a787bea2823f02e1bf4ac65ede63a540515dc4f9721d582af7f1b352"
   end
 
   def install
