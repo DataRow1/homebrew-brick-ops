44d31c691f2ebf6d04b48eb222c10f8f9a8e96bf
Rowan <rowan.weiffenbach@me.com>
2026-01-05 13:30:31 +0100

    Update brick-ops.rb

--- Formula/brick-ops.rb
+++ Formula/brick-ops.rb
@@ -6,22 +6,17 @@ class BrickOps < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DataRow1/db-ops/releases/download/#{version}/dbops-darwin-arm64.tar.gz"
-      sha256 "__MACOS_ARM64_SHA256__"
+      url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/dbops-darwin-arm64.tar.gz"
+      sha256 "29ccb89edab69b67f3df3283a589c1876581dd9556a88f16b49528fbd1b4f074"
     else
-      url "https://github.com/DataRow1/db-ops/releases/download/#{version}/dbops-darwin-amd64.tar.gz"
-      sha256 "__MACOS_ARM64_SHA256__"
+      url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/dbops-darwin-amd64.tar.gz"
+      sha256 "7066372ab0630054609abde338c8fce5fe1b870b6d21e6c0670e0ac789e4a022"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/DataRow1/db-ops/releases/download/#{version}/dbops-linux-arm64.tar.gz"
-      sha256 "__LINUX_ARM64_SHA256__"
-    else
-      url "https://github.com/DataRow1/db-ops/releases/download/#{version}/dbops-linux-amd64.tar.gz"
-      sha256 "__LINUX_AMD64_SHA256__"
-    end
+    url "https://github.com/DataRow1/brick-ops/releases/download/#{version}/dbops-linux-amd64.tar.gz"
+    sha256 "__LINUX_AMD64_SHA256__"
   end
 
   def install
